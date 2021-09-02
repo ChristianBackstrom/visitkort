337c512adbd9a975a7b5f79210fef79a042a966e
ChristianBackstrom <christian.berggren.backstrom@elev.ga.ntig.se>
2021-09-02 13:52:11 +0200

    feat: dotenv added for accesibility to use card

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-var sassMiddleware = require('node-sass-middleware');
+require('dotenv').config();
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const sassMiddleware = require('node-sass-middleware');
 
 var indexRouter = require('./routes/index');
 
