3bdbb0bb556d6a92db10206738c73491d8dea55c
ChristianBackstrom <christian.berggren.backstrom@elev.ga.ntig.se>
2021-09-02 13:52:36 +0200

    feat: variables for text

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { name: process.env.name , tel: process.env.tel_number , mail: process.env.mail , note: process.env.note });
 });
 
 module.exports = router;
