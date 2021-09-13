ddb19dd8dcb8d14b71321947f9ecf7819b2f8405
ChristianBackstrom <christian.berggren.backstrom@elev.ga.ntig.se>
2021-09-13 08:46:16 +0200

    changed from express to raw html

--- routes/index.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { name: process.env.name , tel: process.env.tel_number , mail: process.env.mail , note: process.env.note });
-});
-
-module.exports = router;
